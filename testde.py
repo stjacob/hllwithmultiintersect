96cde9996671c0522c65d32d2316b37efc62daef
stjacob <jichangbin@jideMacBook-Air.local>
2016-09-08 19:43:41 +0800

    format

--- testde.py
+++ testde.py
@@ -3,12 +3,12 @@ import numpy
 from hllcounter import HllCounter
 import pickle
 
-hcpk=open('hc.pk','rb')
-hc1pk=open('hc1.pk','rb')
-hc=pickle.load(hcpk)
-hc1=pickle.load(hc1pk)
+hcpk = open('hc.pk', 'rb')
+hc1pk = open('hc1.pk', 'rb')
+hc = pickle.load(hcpk)
+hc1 = pickle.load(hc1pk)
 print hc.size()
 print hc1.size()
-hcarray=[hc,hc1]
-print "first:",HllCounter.intersect(hcarray)
-print "second:",HllCounter.intersect(hcarray)
+hcarray = [hc, hc1]
+print "first:", HllCounter.intersect(hcarray)
+print "second:", HllCounter.intersect(hcarray)
